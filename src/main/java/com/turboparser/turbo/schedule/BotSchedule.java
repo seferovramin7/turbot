7150f93c67beac53fca935fa5e440f3ce55ca720
seferovramin7 <seferovramin7@gmail.com>
2022-04-13 11:18:42 +0400

    mvp

--- src/main/java/com/turboparser/turbo/schedule/BotSchedule.java
+++ src/main/java/com/turboparser/turbo/schedule/BotSchedule.java
@@ -86,7 +86,7 @@ public class BotSchedule {
 
         for (int i = 0; i < all.size(); i++) {
             if (all.get(i).getCurrency() == null) {
-                System.out.println(all.get(i));
+                System.out.println(all.get(i).getMake());
                 searchParameterRepository.delete(all.get(i));
             }
             System.out.println("-------------------------------------------------------");
