0c4afb7a91808bf225743f2dbbef8d3470eb6dbd
seferovramin7 <seferovramin7@gmail.com>
2022-04-14 17:53:49 +0400

    mvp

--- src/main/java/com/turboparser/turbo/repository/TurboMakeRepository.java
+++ src/main/java/com/turboparser/turbo/repository/TurboMakeRepository.java
@@ -2,12 +2,13 @@ package com.turboparser.turbo.repository;
 
 import com.turboparser.turbo.entity.MakeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface TurboMakeRepository extends JpaRepository<MakeEntity, Long> {
+public interface TurboMakeRepository extends JpaRepository<MakeEntity, Long>, JpaSpecificationExecutor<MakeEntity> {
     @Override
     List<MakeEntity> findAll();
 
