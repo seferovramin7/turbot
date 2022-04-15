59a45bdb37242466afab93301735e43e51e9cf79
seferovramin7 <seferovramin7@gmail.com>
2022-04-15 09:38:37 +0400

    mvp

--- src/main/java/com/turboparser/turbo/service/impl/MessageReceiverServiceImpl.java
+++ src/main/java/com/turboparser/turbo/service/impl/MessageReceiverServiceImpl.java
@@ -589,7 +589,7 @@ public class MessageReceiverServiceImpl implements MessageReceiverService {
     }
 
     private SendMessageDTO getPriceQuestionMessage(Long chatId, Language language, boolean isLowPriceQuestion) {
-        SendMessageDTO sendMessageDTO = getSkippableQuestion(language);
+        SendMessageDTO sendMessageDTO = new SendMessageDTO();
         sendMessageDTO.setChatId(chatId);
         if (isLowPriceQuestion)
             sendMessageDTO.setText(messageProvider.getMessage("question_min_price", language));
@@ -599,7 +599,7 @@ public class MessageReceiverServiceImpl implements MessageReceiverService {
     }
 
     private SendMessageDTO getYearQuestionMessage(Long chatId, Language language, boolean isLowYearQuestion) {
-        SendMessageDTO sendMessageDTO = getSkippableQuestion(language);
+        SendMessageDTO sendMessageDTO = new SendMessageDTO();
         sendMessageDTO.setChatId(chatId);
         if (isLowYearQuestion)
             sendMessageDTO.setText(messageProvider.getMessage("question_min_year", language));
